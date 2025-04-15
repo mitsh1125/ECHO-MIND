eda52a15e61bf7c24f3e74c7f1de27947362010c
KaruSenpai <gealonjhoncarlo@gmail.com>
2025-04-15 09:45:58 +0800

    ECHO MIND

--- app/src/main/java/com/example/echomind/MainActivity.java
+++ app/src/main/java/com/example/echomind/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.echomind;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, create_account.class);
             }
         });
+
     }
 }
+//https://www.youtube.com/watch?v=yndGD5UDX2s&list=PLk7v1Z2rk4hhGfJw-IQCm6kjywmuJX4Rh&index=6
--- app/src/main/java/com/example/echomind/create_account.java
+++ app/src/main/java/com/example/echomind/create_account.java
@@ -1,4 +1,32 @@
 package com.example.echomind;
-
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 public class create_account {
+    public class CreateAccountActivity extends AppCompatActivity {
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.create_account); // Your XML layout
+
+            Button createAccountButton = findViewById(R.id.created_btn);
+
+            // Set OnClickListener
+            createAccountButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Navigate to Log In screen
+                    Intent intent = new Intent(CreateAccountActivity.this,MainActivity.class);
+                    startActivity(intent);
+
+
+                }
+            });
+        }
+    }
+
 }
